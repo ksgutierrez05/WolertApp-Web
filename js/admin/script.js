50ed02cf6dcb38e7702e4230313bae7580d0c2d2
aacadavid <aacadavid@unicesar.edu.co>
2026-09-13 18:36:53 -0500

    feat: creacion de funcion de reloj dinamico

---
+++ js/admin/script.js
@@ -0,0 +1,18 @@
+const fechaSpan = document.getElementById("fecha-actual");
+const horaSpan = document.getElementById("hora-actual");
+
+function actualizarFechaHora() {
+    const ahora = new Date();
+
+    const opcionesFecha = { day: 'numeric', month: 'long', year: 'numeric' };
+    const fechaTexto = ahora.toLocaleDateString('es-ES', opcionesFecha);
+
+    const opcionesHora = { hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    const horaTexto = ahora.toLocaleTimeString('es-ES', opcionesHora);
+
+    fechaSpan.textContent = fechaTexto;
+    horaSpan.textContent = horaTexto;
+}
+
+//funciona para reptetir acciones en itervalos de tiempo en milisegundos
+actualizarFechaHora(actualizarFechaHora,1000);
