3459dbd00aa8883109ba35a5aff4c9de8bd2222a
aacadavid <aacadavid@unicesar.edu.co>
2026-09-14 08:11:06 -0500

    feat: animacion de cambio de menu registro/login

--- js/landing/script.js
+++ js/landing/script.js
@@ -10,4 +10,20 @@ const observer = new IntersectionObserver((entries) => {
 
 pasos.forEach((paso) => observer.observe(paso));
 
+const authContainer = document.getElementById("auth-container");
+const btnToggle = document.getElementById("btn-toggle");
+
+console.log("btnToggle es:", btnToggle);
+
+btnToggle.addEventListener("click", () => {
+    authContainer.classList.toggle("active");
+
+    // Cambia el texto del botón según el estado actual
+    if (authContainer.classList.contains("active")) {
+        btnToggle.textContent = "Iniciar sesión";
+    } else {
+        btnToggle.textContent = "Registrarse";
+    }
+});
+
 console.log("Script cargado correctamente");
